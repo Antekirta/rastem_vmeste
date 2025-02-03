a6b85caac3f0499755080dad72068eca3ea0c8fe
Antekirta <murashkin.kirill.konst@gmail.com>
2025-02-02 21:54:23 -0300

    Tiny fix

--- components/action-button/action-button.js
+++ components/action-button/action-button.js
@@ -12,48 +12,50 @@ function initActionButtons() {
   const $roots = document.querySelectorAll('.action-button')
 
   $roots.forEach($root => {
-    if (!initializedSet.has($root)) {
-      const $svg = $root.querySelector('svg')
-      const { width, height } = $root.getBoundingClientRect();
+    setTimeout(() => {
+      if (!initializedSet.has($root)) {
+        const $svg = $root.querySelector('svg')
+        const { width, height } = $root.getBoundingClientRect();
 
-      if ($svg && width && height) {
-        initializedSet.add($root)
+        if ($svg && width && height) {
+          initializedSet.add($root)
 
-        const roughOptions = {
-          roughness: 2,
-          strokeWidth: 6,
-          stroke: 'rgb(50,33,30)',
-          fill: $root.getAttribute('data-rough-fill') === 'blue' ? 'rgb(149,172,173)' : 'rgb(220,157,138)',
-          fillStyle: $root.getAttribute('data-rough-fill-style') || 'zigzag',
-          hachureGap: 10
-        }
+          const roughOptions = {
+            roughness: 2,
+            strokeWidth: 6,
+            stroke: 'rgb(50,33,30)',
+            fill: $root.getAttribute('data-rough-fill') === 'blue' ? 'rgb(149,172,173)' : 'rgb(220,157,138)',
+            fillStyle: $root.getAttribute('data-rough-fill-style') || 'zigzag',
+            hachureGap: 10
+          }
 
-        $svg.setAttribute('width', $root.offsetWidth)
-        $svg.setAttribute('height', $root.offsetHeight)
+          $svg.setAttribute('width', $root.offsetWidth)
+          $svg.setAttribute('height', $root.offsetHeight)
 
-        let roughSvg = rough.svg($svg);
-        let rect = roughSvg.rectangle(0, 0, width, height, roughOptions);
+          let roughSvg = rough.svg($svg);
+          let rect = roughSvg.rectangle(0, 0, width, height, roughOptions);
 
-        $svg.appendChild(rect);
+          $svg.appendChild(rect);
 
-        if ($root.getAttribute('data-rough-animated')) {
-          setInterval(() => {
-            rect.remove();
+          if ($root.getAttribute('data-rough-animated')) {
+            setInterval(() => {
+              rect.remove();
 
-            roughSvg = rough.svg($svg);
+              roughSvg = rough.svg($svg);
 
-            rect = roughSvg.rectangle(0, 0, width, height, roughOptions);
+              rect = roughSvg.rectangle(0, 0, width, height, roughOptions);
 
-            $svg.appendChild(rect);
-          }, 300)
+              $svg.appendChild(rect);
+            }, 300)
+          }
         }
       }
-    }
 
-    $root.addEventListener('click', () => {
-      const dataModalId = $root.getAttribute('data-modal-id');
+      $root.addEventListener('click', () => {
+        const dataModalId = $root.getAttribute('data-modal-id');
 
-      eventBus.emit(EVENT_SHOW_MODAL, dataModalId)
-    })
+        eventBus.emit(EVENT_SHOW_MODAL, dataModalId)
+      })
+    }, 300)
   })
 }
