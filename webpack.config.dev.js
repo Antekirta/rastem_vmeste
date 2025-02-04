f5331b1bc55f84ebd2a2052719dd4cef2b529985
Antekirta <murashkin.kirill.konst@gmail.com>
2025-02-04 15:02:20 -0300

    Update webpack dev config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: 'development',
@@ -9,5 +10,21 @@ module.exports = merge(common, {
     hot: true,
     open: true,
     static: ['./'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader'
+        ],
+      },
+    ],
   },
 });
